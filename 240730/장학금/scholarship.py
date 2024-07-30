442756b06a9be8c9ed21b38d38a6543e3c438acd
ljjunh <156316110+ljjunh@users.noreply.github.com>
2024-07-30 23:27:13 +0900

    Title: 장학금, Time: 101ms, Memory: 24MB, Status: Passed - CodeTree

--- 240730/장학금/scholarship.py
+++ 240730/장학금/scholarship.py
@@ -1,10 +1,7 @@
-arr = list(map(int, input().split()))
+a,b = map(int, input().split())
 res = 0
-if arr[0] < 90:
-    print(0)
-    quit()
-if arr[1] <95:
-    res += 50000
-else:
+if a >= 90 and b >=95:
     res += 100000
+elif a>= 90 and b >=90:
+    res += 50000
 print(res)
