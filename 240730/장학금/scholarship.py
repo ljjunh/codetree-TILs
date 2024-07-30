054f27dabaf1a1babd03f742ed177ec8ddbf488d
ljjunh <156316110+ljjunh@users.noreply.github.com>
2024-07-30 23:25:39 +0900

    Title: 장학금, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240730/장학금/scholarship.py
+++ 240730/장학금/scholarship.py
@@ -1,11 +1,10 @@
 arr = list(map(int, input().split()))
 res = 0
-
-for i in arr:
-    if i <90:
-        break
-    elif i <95:
-        res += 50000
-    else:
-        res += 100000
+if arr[0] < 90:
+    print(0)
+    quit()
+if arr[1] <95:
+    res += 50000
+else:
+    res += 100000
 print(res)
