c97ba87178225d18203aebd90b43896127eb6c8c
ljjunh <156316110+ljjunh@users.noreply.github.com>
2025-05-15 17:04:30 +0900

    Title: 되돌아오기, Time: 195ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250515/되돌아오기/come-back.js
+++ 250515/되돌아오기/come-back.js
@@ -12,19 +12,19 @@ const dx = [1, -1, 0, 0];
 const dy = [0, 0, -1, 1];
 
 const dir = {
-    E : 0,
-    W : 1,
-    S : 2,
-    N : 3
+    'E' : 0,
+    'W' : 1,
+    'S' : 2,
+    'N' : 3
 }
 
 for (let i = 0; i < n; i++){
-    // 움직여보고 for문 끝날때 초기 위치랑 비교해서 현재 초 넣기
     let [d, v] = moves[i].split(' ');
     v = Number(v);
+
     for (let j = 0; j < v; j++){
-        x = x + (dx[dir[d]]);
-        y = y + (dy[dir[d]]);
+        x += dx[dir[d]];
+        y += dy[dir[d]];
         t++;
         if (i !== 0 && x === 0 && y === 0){
             res = t;
