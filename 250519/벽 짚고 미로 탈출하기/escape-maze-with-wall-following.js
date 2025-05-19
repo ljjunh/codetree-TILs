0ae4af4c1b2d88b8b06bb86a4dbd227119098ab9
ljjunh <156316110+ljjunh@users.noreply.github.com>
2025-05-19 10:40:25 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 5063ms, Memory: 14MB, Status: Time Limit Exceed - Codetree

---
+++ 250519/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const n = Number(input[0]);
+let [y, x] = input[1].split(' ').map(Number);
+const grid = input.slice(2, 2 + n);
+// Please Write your code here.
+
+
+// 시계 방향 순
+const dy = [0, 1, 0, -1];
+const dx = [1, 0, -1, 0];
+
+y--; x--;
+
+let d = 0;
+let cnt = 0;
+
+// const rightWall = grid[y + dy[(d + 1) % 4]][x + dx[(d + 1) % 4]]
+// console.log(rightWall)
+
+while(true){
+
+    if(cnt > n * n) break;
+
+    const rightWall = grid[y + dy[(d + 1) % 4]][x + dx[(d + 1) % 4]]
+
+    if(rightWall === '#'){
+        const ny = y + dy[d];
+        const nx = x + dx[d];
+
+        if(ny < 0 || ny >= n || nx < 0 || nx >= n){
+            cnt++;
+            break;
+        }
+
+        const forwardWall = grid[ny][nx];
+
+        if(forwardWall === '#') d = (d + 4 - 1) % 4;
+        else if(forwardWall === '.'){
+            cnt++;
+            y = y + dy[d];
+            x = x + dx[d];
+        }
+    }
+    else if(rightWall === '.'){
+        d = (d + 1) % 4;
+        cnt++;
+        y = y + dy[d];
+        x = x + dx[d];
+    }
+}
+
+console.log(cnt > n * n ? -1 : cnt);
