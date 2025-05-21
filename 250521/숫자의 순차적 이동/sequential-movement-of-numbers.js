6715b4a21627d783c8306777f7680d3038230fac
ljjunh <156316110+ljjunh@users.noreply.github.com>
2025-05-21 10:47:09 +0900

    Title: 숫자의 순차적 이동, Time: 220ms, Memory: 18MB, Status: Passed - Codetree

--- 250521/숫자의 순차적 이동/sequential-movement-of-numbers.js
+++ 250521/숫자의 순차적 이동/sequential-movement-of-numbers.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [n, m] = input[0].split(' ').map(Number);
-const grid = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
+const grid = input.slice(1, n + 1).map(line => line.trim().split(' ').map(Number));
 
 // Please write your code here.
 
