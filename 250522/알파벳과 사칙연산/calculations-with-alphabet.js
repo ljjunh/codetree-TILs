7194434c082e4933e640854881fea92e548597a3
ljjunh <156316110+ljjunh@users.noreply.github.com>
2025-05-22 12:19:13 +0900

    Title: 알파벳과 사칙연산, Time: 253ms, Memory: 16MB, Status: Passed - Codetree

--- 250522/알파벳과 사칙연산/calculations-with-alphabet.js
+++ 250522/알파벳과 사칙연산/calculations-with-alphabet.js
@@ -4,22 +4,29 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const expression = input[0];
 
 // Please Write your code here.
+const a = [];
 
-const n = (expression.length + 1) / 2;
+for(let i = 0; i < expression.length; i++){
+    if(i % 2 === 0) a.push(expression[i]);
+}
+
+const alpa = [...new Set(a)].sort();
 
 const temp = [];
 let mx = -1e9;
 
 function go(idx){
-    if(idx === n){
-        let res = temp[0];
-        for(let i = 2; i < expression.length; i+=2){
+    if(idx === alpa.length){
+        let res = temp[alpa.indexOf(expression[0])];
+        for(let i = 2; i < expression.length; i++){
+            const idx = alpa.indexOf(expression[i]);
+            const x = temp[idx];
             if(expression[i - 1] === '+'){
-                res += temp[i/2];
+                res += x;
             }else if(expression[i - 1] === '-'){
-                res -= temp[i/2];
+                res -= x;
             }else if(expression[i - 1] === '*'){
-                res *= temp[i/2];
+                res *= x;
             }
         }
         mx = Math.max(mx, res);
