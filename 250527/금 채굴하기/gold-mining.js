d4723c6934bd7e3fa68a09c3d938e73bb318a31d
ljjunh <156316110+ljjunh@users.noreply.github.com>
2025-05-27 18:08:49 +0900

    Title: 금 채굴하기, Time: 169ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250527/금 채굴하기/gold-mining.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const grid = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
+
+// Please Write your code here.
+let maxGold = Number.MIN_SAFE_INTEGER;
+
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < n; j++) {
+    for (let k = 0; k <= n / 2; k++) {
+      let gold = 0;
+
+      for (let x = 0; x < n; x++) {
+        for (let y = 0; y < n; y++) {
+          if (Math.abs(i - x) + Math.abs(j - y) <= k && grid[x][y] === 1)
+            gold++;
+        }
+      }
+
+      const cost = k ** 2 + (k + 1) ** 2 - gold * m;
+
+      if (cost <= 0) maxGold = Math.max(gold, maxGold);
+    }
+  }
+}
+
+console.log(maxGold);
