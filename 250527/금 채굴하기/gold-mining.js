0ea337c7640194f4516c6436f99c8d652eb0404a
ljjunh <156316110+ljjunh@users.noreply.github.com>
2025-05-27 18:09:25 +0900

    Title: 금 채굴하기, Time: 185ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250527/금 채굴하기/gold-mining.js
+++ 250527/금 채굴하기/gold-mining.js
@@ -9,7 +9,7 @@ let maxGold = Number.MIN_SAFE_INTEGER;
 
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < n; j++) {
-    for (let k = 0; k <= n / 2; k++) {
+    for (let k = 0; k <= n; k++) {
       let gold = 0;
 
       for (let x = 0; x < n; x++) {
