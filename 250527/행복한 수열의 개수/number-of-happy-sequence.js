ceb7594b923fab92eac7a4a765306645f5018dec
ljjunh <156316110+ljjunh@users.noreply.github.com>
2025-05-27 17:03:53 +0900

    Title: 행복한 수열의 개수, Time: 174ms, Memory: 15MB, Status: Passed - Codetree

--- 250527/행복한 수열의 개수/number-of-happy-sequence.js
+++ 250527/행복한 수열의 개수/number-of-happy-sequence.js
@@ -10,10 +10,14 @@ let result = 0;
 // 가로 확인
 for (let i = 0; i < n; i++) {
   let maxCount = 1;
-  let count = 1;
+  let count = 0;
 
-  for (let j = 1; j < n; j++) {
-    grid[i][j - 1] === grid[i][j] ? count++ : (count = 1);
+  for (let j = 0; j < n; j++) {
+    if (j === 0) {
+      count++;
+    } else {
+      grid[i][j - 1] === grid[i][j] ? count++ : (count = 1);
+    }
 
     maxCount = Math.max(maxCount, count);
 
@@ -27,10 +31,14 @@ for (let i = 0; i < n; i++) {
 // 세로 확인
 for (let i = 0; i < n; i++) {
   let maxCount = 1;
-  let count = 1;
+  let count = 0;
 
-  for (let j = 1; j < n; j++) {
-    grid[j - 1][i] === grid[j][i] ? count++ : (count = 1);
+  for (let j = 0; j < n; j++) {
+    if (j === 0) {
+      count++;
+    } else {
+      grid[j - 1][i] === grid[j][i] ? count++ : (count = 1);
+    }
 
     maxCount = Math.max(maxCount, count);
 
